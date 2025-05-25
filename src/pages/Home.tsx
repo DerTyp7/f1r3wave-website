df953d5348c8b8fbfadbbd589629cf61c4e33e74
DerTyp7 <janis.meister@outlook.com>
2025-05-25 13:54:00 +0200

    Added landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
 import Header from "@/components/Header";
+import "@styles/Home.scss";
 
 export default function Home() {
 	return (
 		<div>
 			<Header />
+
+			<div className="home">
+				<div className="home-text">
+					<h1 className="home-text__headline">Portfolio</h1>
+					<p className="home-text__paragraph">
+						As a passionate <b>hobby photographer</b> , I capture the unique
+						beauty and atmosphere of various places. Join me on a visual journey
+						through my lens.
+					</p>
+					<a href="/gallery" className="home-button">
+						My Gallery
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 }
