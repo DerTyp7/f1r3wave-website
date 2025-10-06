7446b2fa3de4b45d5496ab920d73592215f0c2df
DerTyp7 <janis.meister@outlook.com>
2025-10-06 20:24:36 +0200

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	output: "standalone",
 	experimental: {
 		serverActions: {
 			bodySizeLimit: "10gb",
 		},
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "upload.wikimedia.org",
-				port: "",
-				pathname: "/wikipedia/commons/**",
-			},
-			{
-				protocol: "https",
-				hostname: "static.vecteezy.com",
-				port: "",
-				pathname: "/**",
-			},
-		],
 	},
 };
 
