78ac9d4149bfa574f588c16283f2bab0bf0629c3
DerTyp7 <janis.meister@outlook.com>
2025-10-06 20:01:51 +0200

    Fixed nextconfig for docker usage

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '70mb',
-    },
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-        port: '',
-        pathname: '/wikipedia/commons/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.vecteezy.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+	output: "standalone",
+	experimental: {
+		serverActions: {
+			bodySizeLimit: "10gb",
+		},
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "upload.wikimedia.org",
+				port: "",
+				pathname: "/wikipedia/commons/**",
+			},
+			{
+				protocol: "https",
+				hostname: "static.vecteezy.com",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
