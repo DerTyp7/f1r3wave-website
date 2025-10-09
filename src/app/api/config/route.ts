fd186a1c3ba47abbf081aab97bb80256393eabef
DerTyp7 <janis.meister@outlook.com>
2025-10-09 18:46:14 +0200

    Clean up code

--- src/app/api/config/route.ts
+++ src/app/api/config/route.ts
@@ -1,12 +1,11 @@
 import { configPath, configTemplate } from '@/const/api';
 import fs from 'fs/promises';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
     const fileBuffer = await fs.readFile(configPath, 'utf8');
     const config = JSON.parse(fileBuffer);
-    console.log(config);
     return NextResponse.json(config, { status: 200 });
   } catch {
     await fs.writeFile(configPath, JSON.stringify(configTemplate, null, 2));
